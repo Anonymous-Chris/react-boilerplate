fe15fdc3e0e84d5eadc82afeff9041463bda4cae
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2021-03-05 14:23:27 -0600

    navigation + logo + tab selection color

---
+++ src/components/pages/Home1.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class Home1 extends Component{
+
+  render(){
+      return(
+      <div>
+          <h1>This page is Home1</h1>
+      </div>
+      ); 
+  }
+}
+
+export default Home1;
